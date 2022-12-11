d609a788c824e97799345867d03d696555717d33
mnmustafakemal <117433842+mnmustafakemal@users.noreply.github.com>
2022-12-11 14:09:20 +0300

    BackgroundVideo

---
+++ VideoBackground/js/main.js
@@ -0,0 +1,13 @@
+const btn = document.querySelector(".switch-btn")
+const video = document.querySelector(".video-container")
+
+btn.addEventListener("click", function(){
+    if(!btn.classList.contains("slide")){
+        btn.classList.add("slide")
+        video.pause()   //todo: If I want to play video, I use play(). Or I want to pause video, use pause()
+    }
+    else{
+        btn.classList.remove("slide")
+        video.play()
+    }
+})
